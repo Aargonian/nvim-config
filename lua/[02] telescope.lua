2f6fd5695635d3dd59dfe6bed73df3e66743d9e6
Aaron Gorodetzky <aaron@nytework.com>
2023-11-04 02:00:06 -0400

    Update telescope to search hidden files

--- lua/[02] telescope.lua
+++ lua/[02] telescope.lua
@@ -21,6 +21,11 @@ end
 
 -- Mostly default options but we also have our own "project management" section which handles them a bit specially...
 require('telescope').setup {
+    pickers = {
+        find_files = {
+            hidden = true
+        }
+    },
     extensions = {
         fzf = {
             fuzzy = true,                    -- false will only do exact matching
