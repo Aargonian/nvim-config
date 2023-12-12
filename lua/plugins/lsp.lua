d5d57bf0fe9db6c91168401833f5a7935df6e2b8
Aaron Gorodetzky <aaron@nytework.com>
2023-12-11 23:52:50 -0500

    Update LSP Capabilities

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -13,7 +13,8 @@ vim.api.nvim_create_autocmd('LspAttach', {
         vim.keymap.set('n', 'gr', vim.lsp.buf.references, keymap_opts)
         vim.keymap.set('n', 'g0', vim.lsp.buf.document_symbol, keymap_opts)
         vim.keymap.set('n', 'gW', vim.lsp.buf.workspace_symbol, keymap_opts)
-        -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, keymap_opts).set('n', '<Leader>a', rt.code_action_group.code_action_group, { buffer = bufnr })
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, keymap_opts)
+        --vim.keymap.set('n', '<Leader>a', rt.code_action_group.code_action_group, { buffer = bufnr })
         vim.keymap.set('n', 'gf', function()
             vim.lsp.buf.format { async = true }
         end, keymap_opts)
