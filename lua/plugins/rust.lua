13e41eeb2375dd53442c111327580e0905d8acb8
Aaron Gorodetzky <aaron@nytework.com>
2024-01-26 20:47:10 -0500

    Make rust-analyzer use clippy instead of check

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,5 +1,18 @@
 return {
     'mrcjkb/rustaceanvim',          -- Tools to make rust-analyzer LSP better
     version = '^4',
-    ft = { 'rust' }
+    ft = { 'rust' },
+    config = function()
+        vim.g.rustaceanvim = {
+            server = {
+                settings = {
+                    ['rust-analyzer'] = {
+                        check = {
+                            command = "clippy",
+                        }
+                    }
+                }
+            }
+        }
+    end
 }
