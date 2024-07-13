d42e8ba30861389d664d9f873ae5f65043386a23
Aaron Gorodetzky <aaron@nytework.com>
2024-07-13 15:48:17 -0400

    Add Transparency Plugin

---
+++ lua/plugins/transparent.lua
@@ -0,0 +1,12 @@
+return {
+    'xiyaowong/transparent.nvim',          -- Tools to make rust-analyzer LSP better
+    opts = {
+        extra_gropus = {
+            "NormalFloat",
+            "NvimTreeNormal",
+            "NvimTree",
+        },
+    },
+    config = function()
+    end
+}
