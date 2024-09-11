e482e49362e8cc8d9974ba552b918404b6730a24
Aaron Helton <AGorodetzky@republicbank.com>
2024-09-11 14:41:59 -0400

    Remove git batch

--- Git.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call "C:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvarsall.bat" amd64
-"C:\Users\agorodetzky\AppData\Local\Programs\Git\git-bash.exe" -i
