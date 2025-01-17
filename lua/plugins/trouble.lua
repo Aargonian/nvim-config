74948855ac0b17d9de74ac13cb6b4574c37fdbbf
Aaron Gorodetzky <aaron@nytework.com>
2025-01-16 20:45:26 -0500

    Fix trouble loading

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,11 +1,23 @@
 return {
-    'folke/trouble.nvim',
+    "folke/trouble.nvim",
     tag = "v3.6.0",
+    event = "LspAttach",
+    priority = 2000,
     opts = {
         modes = {
             diagnostics = {
                 auto_open = true,
+                auto_close = false,
+                open_no_results = true,
             },
+            symbols = {
+                auto_open = true,
+                auto_close = false,
+                open_no_results = true,
+                win = {
+                    position = "right"
+                },
+            }
         },
     },
     cmd = "Trouble",
@@ -13,7 +25,32 @@ return {
         {
           "<leader>xx",
           "<cmd>Trouble diagnostics toggle<cr>",
-          desc = "Diagnostics (Trouble)",
+            desc = "Diagnostics (Trouble)",
         },
-    }
+        {
+          "<leader>xX",
+          "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+            desc = "Buffer Diagnostics (Trouble)",
+        },
+        {
+          "<leader>cs",
+          "<cmd>Trouble symbols toggle focus=false<cr>",
+            desc = "Symbols (Trouble)",
+        },
+        {
+          "<leader>cl",
+          "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+            desc = "LSP Definitions / references / ... (Trouble)",
+        },
+        {
+          "<leader>xL",
+          "<cmd>Trouble loclist toggle<cr>",
+            desc = "Location List (Trouble)",
+        },
+        {
+          "<leader>xQ",
+          "<cmd>Trouble qflist toggle<cr>",
+            desc = "Quickfix List (Trouble)",
+        },
+    },
 }
