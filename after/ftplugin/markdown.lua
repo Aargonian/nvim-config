41270e8e38460f678d453c28bbeef6d5406f0a6b
Aaron Gorodetzky <aaron@nytework.com>
2025-01-16 22:41:27 -0500

    Less aggressive Markdown settings

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -33,28 +33,5 @@ opt.tabstop = 4
 opt.conceallevel = 2
 opt.concealcursor = "n"
 
--- -- Markdown Specific Mappings (add your mappings here)
-
--- -- Filetype-Specific Commands (add any commands you want to run for Markdown files)
-
--- -- Modeline Settings
--- opt.modeline = false
-
--- Set background and foreground for Markdown files
-vim.cmd [[
-  highlight Normal guibg=#000000 guifg=#FFFFFF ctermbg=0 ctermfg=15
-  highlight htmlBold guifg=#fabd2f ctermfg=214
-  highlight htmlItalic guifg=#b8bb26 ctermfg=142
-  highlight mkdLink guifg=#ffff00 gui=bold ctermfg=226 cterm=bold
-]]
-
--- Auto format long lines on opening Markdown files
-vim.api.nvim_create_autocmd("BufReadPost", {
-  pattern = "*.md",
-  callback = function()
-    vim.cmd("normal gggqG")
-  end,
-})
-
 -- Set spell checking for Markdown files
 vim.opt.spell = true
