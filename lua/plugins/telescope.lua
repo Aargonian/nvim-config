631f7a8f33af58d12b75437a564fb23bcbe7a3f9
Aaron Gorodetzky <aaron@nytework.com>
2025-05-07 11:47:37 -0400

    Update lazy-lock and fix telescope build (again)

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,7 +11,7 @@ return {
     {
         'Aargonian/telescope-fzf-native.nvim',
         --build = 'make',
-        build = 'cmake --install build --prefix build'
+        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
     },
 
     'nvim-telescope/telescope-file-browser.nvim',
