26fc8d30169bf7f8f7d9976bf895b1359e29775d
Aaron Gorodetzky <aaron@nytework.com>
2025-06-13 13:17:01 -0400

    Readd inline diags

---
+++ lua/plugins/tiny-inline-diagnostics.lua
@@ -0,0 +1,24 @@
+return {
+    'rachartier/tiny-inline-diagnostic.nvim',
+    config = function()
+        require('tiny-inline-diagnostic').setup {
+            options = {
+                throttle = 0,
+                multiple_diag_under_cursor = true,
+                show_all_diags_on_cursorline = true,
+                enable_on_insert = true,
+                multilines = {
+                    enabled = true,
+                    always_show = true,
+                },
+                overflow = {
+                    mode = "none",
+                },
+                break_line = {
+                    enabled = true,
+                },
+            },
+        }
+        vim.diagnostic.config({ virtual_text = false })
+    end
+}
