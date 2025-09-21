70981fb8b9be3dcc9b671b8242b4f0371da3ea26
Aaron Gorodetzky <agorodetzky@republicbank.com>
2025-09-21 18:15:50 -0400

    Update lsp structure

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,146 +1,136 @@
--- Setup Keybindings for LSP on attach. Must be defined on attach.
-vim.api.nvim_create_autocmd('LspAttach', {
-    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-    callback = function(ev)
-        -- Setup Inlay Hints
-        -- Currently Requires Neovim >0.10 (Nightly)
-        --vim.lsp.inlay_hint.enable(ev.buf, true)
-        vim.lsp.inlay_hint.enable(true)
+-- Code to run whenever a language server attaches to a buffer
+local on_attach = function(lsp_client, buffer_number)
+    vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+        callback = function(ev)
+            -- Setup Inlay Hints
+            -- Currently Requires Neovim >0.10 (Nightly)
+            --vim.lsp.inlay_hint.enable(ev.buf, true)
+            vim.lsp.inlay_hint.enable(true)
 
-        -- Setup Signs
-        vim.diagnostic.config({
-            signs = {
-                text = {
-                    [vim.diagnostic.severity.ERROR] = "🛑",
-                    [vim.diagnostic.severity.WARN]  = "⚠️",
-                    [vim.diagnostic.severity.HINT]  = "💡",
-                    [vim.diagnostic.severity.INFO]  = "ℹ️"
-                }
-            }
-        })
-
-        -- Setup Keybinds
-        local keymap_opts = { buffer = ev.buf }
-        vim.keymap.set('n', 'g[', vim.diagnostic.goto_prev, keymap_opts)
-        vim.keymap.set('n', 'g]', vim.diagnostic.goto_next, keymap_opts)
-        vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, keymap_opts)
-        vim.keymap.set('n', 'gh', vim.lsp.buf.hover, keymap_opts)
-        vim.keymap.set('i', '<C-Space>', vim.lsp.buf.hover, keymap_opts)
-        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, keymap_opts)
-        vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help, keymap_opts)
-        vim.keymap.set('n', 'gD', vim.lsp.buf.type_definition, keymap_opts)
-        vim.keymap.set('n', 'gr', vim.lsp.buf.references, keymap_opts)
-        vim.keymap.set('n', 'g0', vim.lsp.buf.document_symbol, keymap_opts)
-        vim.keymap.set('n', 'gW', vim.lsp.buf.workspace_symbol, keymap_opts)
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, keymap_opts)
-        --vim.keymap.set('n', '<Leader>a', rt.code_action_group.code_action_group, { buffer = bufnr })
-        vim.keymap.set('n', 'gf', function()
-            vim.lsp.buf.format { async = true }
-        end, keymap_opts)
-    end,
-})
-
-
-return {
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-            "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig.nvim",
-            {
-                "j-hui/fidget.nvim",
-                tag = "legacy",
-                event = "LspAttach",
-            },
-            "hrsh7th/cmp-nvim-lsp",
-        },
-        opts = {
-            userLanguages = {
-                rust = "html",
-            },
-            inlay_hints = {
-                enabled = true
-            },
-        },
-
-        config = function()
-            require("mason").setup({
-                ui = {
-                    icons = {
-                        package_installed = "",
-                        package_pending = "",
-                        package_uninstalled = "",
-                    },
-                }
-            })
-
-            require("mason-lspconfig").setup{
-                ensure_installed = {
-                    "lua_ls",
-                    "pylsp",
-                    "omnisharp",
-                }
-            }
-
-            -- LSP Status Information
-            require('fidget').setup()
-
-            -- Setup Necessary Parameters for LSP
-            require('lspconfig').ts_ls.setup({
-                init_options = {
-                    preferences = {
-                        includeInlayParameterNameHints = 'all',
-                        includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-                        includeInlayFunctionParameterTypeHints = true,
-                        includeInlayVariableTypeHints = true,
-                        includeInlayPropertyDeclarationTypeHints = true,
-                        includeInlayFunctionLikeReturnTypeHints = true,
-                        includeInlayEnumMemberValueHints = true,
-                        importModuleSpecifierPreference = 'non-relative',
-                    },
-                },
-            })
-
-            local servers = require('lspconfig')
-
-            -- Setup TailwindCSS server
-            servers.tailwindcss.setup({})
-
-            -- Setup NIX
-            servers.nil_ls.setup({})
-
-            -- Setup Bash Server
-            servers.bashls.setup({
-                filetypes = { 'bash', 'sh', 'zsh' }
-            })
-
-            -- Setup Lua
-            servers.lua_ls.setup({
-                settings = {
-                    Lua = {
-                        diagnostics = {
-                            globals = { 'vim' },
-                            disable = { 'missing-fields' },
-                        },
-                        runtime = {
-                            version = "LuaJIT",
-                        },
-                        workspace = {
-                            library = vim.api.nvim_get_runtime_file("", true),
-                        },
+            -- Setup Signs
+            vim.diagnostic.config({
+                signs = {
+                    text = {
+                        [vim.diagnostic.severity.ERROR] = "🛑",
+                        [vim.diagnostic.severity.WARN]  = "⚠️",
+                        [vim.diagnostic.severity.HINT]  = "💡",
+                        [vim.diagnostic.severity.INFO]  = "ℹ️"
                     }
                 }
             })
 
-            -- Setup HTML
-            local capabilities = vim.lsp.protocol.make_client_capabilities()
-            capabilities.textDocument.completion.completionItem.snippetSupport = true
-            require('lspconfig').html.setup({ capabilities = capabilities, })
+            -- Setup Keybinds
+            local keymap_opts = { buffer = ev.buf }
+            vim.keymap.set('n', 'g[', vim.diagnostic.goto_prev, keymap_opts)
+            vim.keymap.set('n', 'g]', vim.diagnostic.goto_next, keymap_opts)
+            vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, keymap_opts)
+            vim.keymap.set('n', 'gh', vim.lsp.buf.hover, keymap_opts)
+            vim.keymap.set('i', '<C-Space>', vim.lsp.buf.hover, keymap_opts)
+            vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, keymap_opts)
+            vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help, keymap_opts)
+            vim.keymap.set('n', 'gD', vim.lsp.buf.type_definition, keymap_opts)
+            vim.keymap.set('n', 'gr', vim.lsp.buf.references, keymap_opts)
+            vim.keymap.set('n', 'g0', vim.lsp.buf.document_symbol, keymap_opts)
+            vim.keymap.set('n', 'gW', vim.lsp.buf.workspace_symbol, keymap_opts)
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, keymap_opts)
+            --vim.keymap.set('n', '<Leader>a', rt.code_action_group.code_action_group, { buffer = bufnr })
+            vim.keymap.set('n', 'gf', function()
+                vim.lsp.buf.format { async = true }
+            end, keymap_opts)
+        end,
+    })
+end
 
-            -- Setup Omnisharp
-            -- require('lspconfig').omnisharp.setup {
-            --     capabilities = capabilities
-            -- }
+local servers = {
+    pylsp = {},
+    omnisharp = {},
+    html = {},
+    lua_ls = {
+        settings = {
+            Lua = {
+                diagnostics = {
+                    globals = { 'vim' },
+                    disable = { 'missing-fields' },
+                },
+                runtime = {
+                    version = "LuaJIT",
+                },
+                workspace = {
+                    library = vim.api.nvim_get_runtime_file("", true),
+                },
+            },
+        },
+    },
+    tailwindcss = {},
+    -- nil_ls = {},
+    bashls = {
+        filetypes = { 'bash', 'sh', 'zsh' }
+    },
+    ts_ls = {
+        init_options = {
+            preferences = {
+                includeInlayParameterNameHints = 'all',
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+                importModuleSpecifierPreference = 'non-relative',
+            },
+        },
+    },
+}
+
+return {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        {
+            "j-hui/fidget.nvim",
+            tag = "legacy",
+            event = "LspAttach",
+        },
+        "hrsh7th/cmp-nvim-lsp",
+    },
+    opts = {
+        inlay_hints = {
+            enabled = true
+        },
+    },
+
+    config = function()
+        require("mason").setup({
+            ui = {
+                icons = {
+                    package_installed = "",
+                    package_pending = "",
+                    package_uninstalled = "",
+                },
+            }
+        })
+
+        -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+        capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+        require("mason-lspconfig").setup {
+            ensure_installed = vim.tbl_keys(servers),
+        }
+
+        for name, conf in pairs(servers) do
+            local cfg = vim.tbl_deep_extend("force", {
+                capabilities = capabilities,
+                on_attach = on_attach,
+            }, conf)
+
+            vim.lsp.config(name, cfg)
         end
-    }
+
+        -- LSP Status Information
+        require('fidget').setup()
+    end
 }
