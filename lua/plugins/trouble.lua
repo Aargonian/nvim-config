c5db990cc566fe6d2ab3efc9c9e162bb07c5cf3a
Aaron Gorodetzky <aaron@nytework.com>
2025-10-23 12:22:44 -0400

    Clean up some comments

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -35,30 +35,5 @@ return {
           "<cmd>Trouble symbols toggle focus=false<cr>",
             desc = "Symbols (Trouble)",
         },
-        -- {
-        --   "<leader>c",
-        --   "<cmd>Trouble lsp_document_symbols toggle focus=false<cr>",
-        --     desc = "Symbols (Trouble)",
-        -- },
-        -- {
-        --   "<leader>xX",
-        --   "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-        --     desc = "Buffer Diagnostics (Trouble)",
-        -- },
-        -- {
-        --   "<leader>cl",
-        --   "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-        --     desc = "LSP Definitions / references / ... (Trouble)",
-        -- },
-        -- {
-        --   "<leader>xL",
-        --   "<cmd>Trouble loclist toggle<cr>",
-        --     desc = "Location List (Trouble)",
-        -- },
-        -- {
-        --   "<leader>xQ",
-        --   "<cmd>Trouble qflist toggle<cr>",
-        --     desc = "Quickfix List (Trouble)",
-        -- },
     },
 }
